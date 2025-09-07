b2e3bf8b377ee2665326a913eeba7f627ef55423
phenix06 <phenix06@gmail.com>
2025-09-07 23:09:04 +0200

    v3.2

--- etc/iptables-nat.sh
+++ etc/iptables-nat.sh
@@ -14,29 +14,29 @@ iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 
 # Autoriser le trafic du LAN vers WAN
-iptables -A FORWARD -i ens36 -o ens33 -j ACCEPT
+iptables -A FORWARD -i ens33 -o ens32 -j ACCEPT
 
 # Autoriser ICMP (ping) depuis le LAN
-iptables -A INPUT -i ens36 -p icmp -j ACCEPT
-iptables -A FORWARD -i ens36 -o ens33 -p icmp -j ACCEPT
+iptables -A INPUT -i ens33 -p icmp -j ACCEPT
+iptables -A FORWARD -i ens33 -o ens32 -p icmp -j ACCEPT
 
 # Services réseau sur LAN
-iptables -A INPUT -i ens36 -p udp --dport 67 -j ACCEPT  # DHCP
-iptables -A INPUT -i ens36 -p udp --dport 53 -j ACCEPT  # DNS
-iptables -A INPUT -i ens36 -p tcp --dport 53 -j ACCEPT  # DNS
+iptables -A INPUT -i ens33 -p udp --dport 67 -j ACCEPT  # DHCP
+iptables -A INPUT -i ens33 -p udp --dport 53 -j ACCEPT  # DNS
+iptables -A INPUT -i ens33 -p tcp --dport 53 -j ACCEPT  # DNS
 
 # Services web sur LAN
-iptables -A INPUT -i ens36 -p tcp --dport 80 -j ACCEPT   # HTTP
-iptables -A INPUT -i ens36 -p tcp --dport 443 -j ACCEPT  # HTTPS
+iptables -A INPUT -i ens33 -p tcp --dport 80 -j ACCEPT   # HTTP
+iptables -A INPUT -i ens33 -p tcp --dport 443 -j ACCEPT  # HTTPS
 
 # Cockpit (port 9090)
-iptables -A INPUT -i ens36 -p tcp --dport 9090 -j ACCEPT
+iptables -A INPUT -i ens33 -p tcp --dport 9090 -j ACCEPT
 
 # SSH
-iptables -A INPUT -i ens36 -p tcp --dport 22 -j ACCEPT
+iptables -A INPUT -i ens33 -p tcp --dport 22 -j ACCEPT
 
 # NAT : masquerading pour le trafic sortant
-iptables -t nat -A POSTROUTING -o ens33 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o ens32 -j MASQUERADE
 
 # Politique par défaut
 iptables -P FORWARD DROP
--- root/install.sh
+++ root/install.sh
@@ -1,13 +1,15 @@
-cp -r /root/holododeck/* /
-
+cd /root/holodeck
+cp etc/apt/sources.list.d/*  /etc/apt/sources.list.d
+cp usr/share/keyrings/* /usr/share/keyrings
 apt update
-
-chmod +x /etc/iptables-nat.sh
-
 apt install proftpd-core  proftpd-mod-crypto wget -y
-apt install dnsmasq iptables iptables-persistent lsb-release apt-transport-https ca-certificates wget nginx mariadb-server  unzip -y
-apt install php7.4-fpm php7.4-mysql php7.4-cli php7.4-curl php7.4-gd php7.4-mbstring php7.4-xml php7.4-zip php7.4-mysql  php8.4-fpm php8.4-mysql php8.4-cli php8.4-curl php8.4-gd php8.4-mbstring php8.4-xml php8.4-zip php8.4-mysql  cockpit lynx -y
+apt install dnsmasq iptables iptables-persistent lsb-release apt-transport-https ca-certificates wget nginx mariadb-server  unzip  cockpit lynx -y
+apt install php7.4-fpm php7.4-mysql php7.4-cli php7.4-curl php7.4-gd php7.4-mbstring php7.4-xml php7.4-zip php7.4-mysql -y  
+apt install php8.4-fpm php8.4-mysql php8.4-cli php8.4-curl php8.4-gd php8.4-mbstring php8.4-xml php8.4-zip php8.4-mysql -y
 
+cp -r /root/holodeck/* /
+chmod +x /etc/iptables-nat.sh
+sed -i "s/user  nginx;/user  www-data;/g"  /etc/nginx/nginx.conf
 
 cd /etc/ssl/starfleet
 # Créer la clé privée de la CA
@@ -20,6 +22,10 @@ wget https://files.phpmyadmin.net/phpMyAdmin/5.2.2/phpMyAdmin-5.2.2-all-language
 unzip -q phpMyAdmin-5.2.2-all-languages.zip
 mv phpMyAdmin-5.2.2-all-languages phpMyAdmin
 rm *.zip
+
+mkdir /var/www/certificates
+cp /etc/ssl/starfleet/starfleet-ca.crt /var/www/certificates/.
+
 
 chown -R www-data: /var/www/phpMyAdmin
 cp /var/www/phpMyAdmin/config.sample.inc.php /var/www/phpMyAdmin/config.inc.php
