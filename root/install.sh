f1480d8dbff2e2a5838a6f89ff9cf1c213aedbe9
phenix06 <phenix06@gmail.com>
2025-09-07 19:02:28 +0200

    v3

--- root/install.sh
+++ root/install.sh
@@ -1,2 +1,32 @@
-apt install dnsmasq iptables iptables-persistent lsb-release apt-transport-https ca-certificates wget nginx mariadb-server 
-apt install php7.4-fpm php7.4-mysql php7.4-cli php7.4-curl php7.4-gd php7.4-mbstring php7.4-xml php7.4-zip php7.4-mysql  php8.4-fpm php8.4-mysql php8.4-cli php8.4-curl php8.4-gd php8.4-mbstring php8.4-xml php8.4-zip php8.4-mysql  cockpit lynx
+cp -r /root/holododeck/* /
+
+apt update
+
+chmod +x /etc/iptables-nat.sh
+
+apt install proftpd-core  proftpd-mod-crypto wget -y
+apt install dnsmasq iptables iptables-persistent lsb-release apt-transport-https ca-certificates wget nginx mariadb-server  unzip -y
+apt install php7.4-fpm php7.4-mysql php7.4-cli php7.4-curl php7.4-gd php7.4-mbstring php7.4-xml php7.4-zip php7.4-mysql  php8.4-fpm php8.4-mysql php8.4-cli php8.4-curl php8.4-gd php8.4-mbstring php8.4-xml php8.4-zip php8.4-mysql  cockpit lynx -y
+
+
+cd /etc/ssl/starfleet
+# Créer la clé privée de la CA
+openssl genrsa -out starfleet-ca.key 4096
+openssl req -new -x509 -days 3650 -key starfleet-ca.key -out starfleet-ca.crt -subj "/C=FR/ST=PACA/L=Marseille/O=Starfleet Command/OU=IT Department/CN=Starfleet Root CA"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048     -keyout starfleet-wildcard.key     -out starfleet-wildcard.crt     -config starfleet-wildcard.conf
+
+cd /var/www
+wget https://files.phpmyadmin.net/phpMyAdmin/5.2.2/phpMyAdmin-5.2.2-all-languages.zip
+unzip -q phpMyAdmin-5.2.2-all-languages.zip
+mv phpMyAdmin-5.2.2-all-languages phpMyAdmin
+rm *.zip
+
+chown -R www-data: /var/www/phpMyAdmin
+cp /var/www/phpMyAdmin/config.sample.inc.php /var/www/phpMyAdmin/config.inc.php
+
+blow=$(openssl rand -base64 32 | cut -c1-32)
+sed -i "s/\$cfg\['blowfish_secret'\] = '.*';/\$cfg['blowfish_secret'] = '$blow';/" /var/www/phpMyAdmin/config.inc.php
+
+mariadb -u root < /root/maria.sql
+
+systemctl enable iptables-nat.service
